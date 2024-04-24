0234b9632c83eb79e5df1e61e060dbac450fc982
stsaria <solothunder.autoer@gmail.com>
2024-04-24 09:28:25 +0900

    fix video list

--- cgi-bin/videoList.php
+++ cgi-bin/videoList.php
@@ -10,14 +10,12 @@
             }
             if ($rows && $all == false){if (count($rows) >= 30){$rows = array_slice($rows, -30);}}
             $rows = array_reverse($rows);
-            if (!empty($rows)){echo "<ul>";}
             foreach ($rows as $row){
                 $link = "watch.php?videoId=".$row[0];
-                echo "<li>".$row[2]."|".$row[3]."</br><a href=".$link.">".$row[1];
+                echo "<li style=\"margin-right: 100px;\">".$row[2]."|ID:".$row[3]."</br><a href=".$link.">".$row[1];
                 if(!file_exists("videos/hls/".$row[0].".m3u8")){echo "(処理中)";}
-                echo "</a></li><p style=\"margin-right: 140px;\">";
+                echo "</a></li>";
             }
-            echo "</ul>";
             flock($fp, LOCK_UN);
         } else {
             echo '<script>alert("File lock failed.");</script>';
